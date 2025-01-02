bef70ac88ad68a97ec5ba7a93a2bd57f4fe54b61
Semih Ünal <semih1837@gmail.com>
2025-01-03 00:11:41 +0300

    icon feature implemented

--- prettyconsole.go
+++ prettyconsole.go
@@ -1,6 +1,8 @@
 package prettyconsole
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	ColorReset  = "\033[0m"
@@ -10,24 +12,113 @@ const (
 	ColorBlue   = "\033[34m"
 )
 
-type Logger struct{}
+var defaultLogColors = LogColors{
+	Info:    "\033[34m",
+	Success: "\033[92m",
+	Warning: "\033[33m",
+	Error:   "\033[31m",
+	Reset:   "\033[0m",
+}
 
-func NewLogger() *Logger {
-	return &Logger{}
+type LogColors struct {
+	Reset   string
+	Error   string
+	Warning string
+	Info    string
+	Success string
+}
+
+type Icons struct {
+	Error   string
+	Warning string
+	Info    string
+	Success string
+}
+
+var icons = Icons{
+	Error:   "❌",
+	Warning: "⚠️",
+	Info:    "📝",
+	Success: "✅",
+}
+
+type LoggerOptions struct {
+	ShowIcons bool
+}
+
+type Logger struct {
+	colors  LogColors
+	options map[string]bool
+}
+
+func NewLogger(options map[string]bool) *Logger {
+	loggerOptions := map[string]bool{
+		"ShowIcons": true,
+	}
+
+	for key, value := range options {
+		loggerOptions[key] = value
+	}
+
+	return &Logger{
+		colors:  defaultLogColors,
+		options: loggerOptions,
+	}
+}
+
+func (logger *Logger) log(logType string, message string) {
+	color := logger.getLogTypeColor(logType)
+	logIcon := ""
+
+	if logger.options["ShowIcons"] {
+		logIcon = "" + getLogIcon(logType) + "  "
+	}
+
+	fmt.Printf("%s%s[%s] %s%s\n", color, logIcon, logType, message, ColorReset)
+}
+
+func getLogIcon(logType string) string {
+	switch logType {
+	case "ERROR":
+		return icons.Error
+	case "WARNING":
+		return icons.Warning
+	case "INFO":
+		return icons.Info
+	case "SUCCESS":
+		return icons.Success
+	default:
+		return ""
+	}
+}
+
+func (logger *Logger) getLogTypeColor(logType string) string {
+	switch logType {
+	case "ERROR":
+		return logger.colors.Error
+	case "WARNING":
+		return logger.colors.Warning
+	case "INFO":
+		return logger.colors.Info
+	case "SUCCESS":
+		return logger.colors.Success
+	default:
+		return logger.colors.Reset
+	}
 }
 
 func (logger *Logger) Success(message string) {
-	fmt.Printf("%s[SUCCESS] %s%s\n", ColorGreen, message, ColorReset)
+	logger.log("SUCCESS", message)
 }
 
 func (logger *Logger) Info(message string) {
-	fmt.Printf("%s[INFO] %s%s\n", ColorBlue, message, ColorReset)
+	logger.log("INFO", message)
 }
 
 func (logger *Logger) Warning(message string) {
-	fmt.Printf("%s[WARNING] %s%s\n", ColorYellow, message, ColorReset)
+	logger.log("WARNING", message)
 }
 
 func (logger *Logger) Error(message string) {
-	fmt.Printf("%s[ERROR] %s%s\n", ColorRed, message, ColorReset)
+	logger.log("ERROR", message)
 }
